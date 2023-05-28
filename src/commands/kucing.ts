5427ab16096d22963148334ef96c6e41ce51ec08
hendraaagil <hendraaagil@gmail.com>
2023-05-28 14:14:17 +0700

    feat: change cat api

--- src/commands/kucing.ts
+++ src/commands/kucing.ts
@@ -1,11 +1,16 @@
+import axios from 'axios';
 import { Client, Message, MessageMedia } from 'whatsapp-web.js';
 import { PREFIX } from '../constants';
 
 export const kucing = {
   command: PREFIX + 'kucing',
   execute: async (message: Message, client: Client) => {
-    await client.sendMessage(message.from, '🔎 Lagi nyari kucing...');
-    const image = await MessageMedia.fromUrl('https://cataas.com/cat', {
+    const [response] = await Promise.all([
+      axios.get('https://api.thecatapi.com/v1/images/search'),
+      client.sendMessage(message.from, '🔎 Lagi nyari kucing...'),
+    ]);
+
+    const image = await MessageMedia.fromUrl(response.data[0]?.url, {
       unsafeMime: true,
     });
     return client.sendMessage(message.from, image);
